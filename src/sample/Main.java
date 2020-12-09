61f920278ccc9613502b9512662e17a4be46ba64
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2020-12-09 21:57:12 +0500

    9:57 pm

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,22 +2,49 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        AnchorPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        //for getting the screen size
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        primaryStage.setX(bounds.getMinX());//
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());//
+
+        KeyCombination keyCombinationWin1 = new KeyCodeCombination(KeyCode.TAB, KeyCombination.ALT_ANY);
+        KeyCombination keyCombinationWin2 = new KeyCodeCombination(KeyCode.WINDOWS, KeyCombination.ALT_ANY);
+
+//        root.addEventHandler
+        Scene scene = new  Scene(root);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (keyCombinationWin1.match(event) || keyCombinationWin2.match(event) ||
+                    event.getCode() == KeyCode.WINDOWS || event.getCode() == KeyCode.ALT ) {
+
+            }
+        });
+        primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
+  }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) throws Exception {launch(args);}
 }
