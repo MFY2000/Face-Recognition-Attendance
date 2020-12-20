4e3e9a836628db264b323fb1c660cf5bc849db69
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2020-12-20 16:16:31 +0500

    4:16 pm

--- src/sample/FXHelloCVController.java
+++ src/sample/FXHelloCVController.java
@@ -1,11 +1,18 @@
 package sample;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import org.opencv.core.Mat;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.WritableImage;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 import org.opencv.videoio.VideoCapture;
 
 import sample.library.Untils;
@@ -23,19 +30,62 @@ public class FXHelloCVController{
     private VideoCapture capture = new VideoCapture();
     private boolean cameraActive = false;
     private static int cameraId = 1;
+    Mat matrix = null;
+    private int absoluteFaceSize = 0;
+    private CascadeClassifier faceCascade = new CascadeClassifier();
+
 
     public void initialize() throws Exception {
         startCamera();
     }
 
+
+    public WritableImage capureSnapShot() {
+        WritableImage WritableImage = null;
+
+        // Loading the OpenCV core library
+        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+
+        // Instantiating the VideoCapture class (camera:: 0)
+        VideoCapture capture = new VideoCapture(cameraId);
+
+        // Reading the next video frame from the camera
+        Mat matrix = new Mat();
+        capture.read(matrix);
+
+        // If camera is opened
+        if( capture.isOpened()) {
+            // If there is next video frame
+            if (capture.read(matrix)) {
+                // Creating BuffredImage from the matrix
+                BufferedImage image = new BufferedImage(matrix.width(),
+                        matrix.height(), BufferedImage.TYPE_3BYTE_BGR);
+
+                WritableRaster raster = image.getRaster();
+                DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+                byte[] data = dataBuffer.getData();
+                matrix.get(0, 0, data);
+                this.matrix = matrix;
+
+                // Creating the Writable Image
+                WritableImage = SwingFXUtils.toFXImage(image, null);
+            }
+        }
+        return WritableImage;
+    }
+
     @FXML protected void startCamera() {
         if (!this.cameraActive) {
-            capture.open(cameraId);
+            // start the video capture
+            String classifierPath = "C:\\Users\\MF\\IdeaProjects\\FaceRecognition&Attendance\\src\\sample\\library\\lbpcascade_frontalface.xml";
+            this.faceCascade.load(classifierPath);
+
             if (this.capture.isOpened()) {
                 this.cameraActive = true;
-
                 Runnable frameGrabber = new Runnable() {
-                    @Override public void run() {
+
+                    @Override
+                    public void run(){
                         Mat frame = grabFrame();
                         Image imageToShow = Untils.mat2Image(frame);
                         updateImageView(currentFrame, imageToShow);
@@ -43,7 +93,7 @@ public class FXHelloCVController{
                 };
 
                 this.timer = Executors.newSingleThreadScheduledExecutor();
-                this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+                this.timer.scheduleAtFixedRate(frameGrabber, 0, 100, TimeUnit.MILLISECONDS);
                 this.button.setText("Stop Camera");
             }
             else {
@@ -57,50 +107,100 @@ public class FXHelloCVController{
         }
     }
 
-    private Mat grabFrame(){
+
+
+    private Mat grabFrame()
+    {
+        // init everything
         Mat frame = new Mat();
 
-        if (this.capture.isOpened()) {
+        // check if the capture is open
+        if (this.capture.isOpened())
+        {
             try {
                 this.capture.read(frame);
+                if (!frame.empty()) {
+                    MatOfRect faces = new MatOfRect();
+                    Mat grayFrame = new Mat();
 
-               if (!frame.empty())
-                    Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+                    // convert the frame in gray scale
+                    Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+                    // equalize the frame histogram to improve the result
+                    Imgproc.equalizeHist(grayFrame, grayFrame);
+
+                    // compute minimum face size (20% of the frame height, in our case)
+                    if (this.absoluteFaceSize == 0) {
+                        int height = grayFrame.rows();
+                        if (Math.round(height * 0.2f) > 0) {
+                            this.absoluteFaceSize = Math.round(height * 0.2f);
+                        }
+                    }
+
+                    // detect faces
+                    System.out.println("hi1");
+                    this.faceCascade.detectMultiScale(grayFrame, faces, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE, new Size(this.absoluteFaceSize, this.absoluteFaceSize), new Size());
+
+                    // each rectangle in faces is a face: draw them!
+                    Rect[] facesArray = faces.toArray();
+                    for (int i = 0; i < facesArray.length; i++)
+                        Imgproc.rectangle(frame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0), 3);}
+
             }
-            catch (Exception e) {
-                 System.err.println("Exception during the image elaboration: " + e);
+            catch (Exception e)
+            {
+                System.err.println("Exception during the image elaboration: " + e);
             }
         }
 
         return frame;
     }
 
-    private void stopAcquisition() {
-        if (this.timer!=null && !this.timer.isShutdown()) {
-            try {
+    /**
+     * Stop the acquisition from the camera and release all the resources
+     */
+    private void stopAcquisition()
+    {
+        if (this.timer!=null && !this.timer.isShutdown())
+        {
+            try
+            {
+                // stop the timer
                 this.timer.shutdown();
                 this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
             }
-            catch (Exception e){
-               System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+            catch (InterruptedException e)
+            {
+                // log any exception
+                System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
             }
         }
 
         if (this.capture.isOpened())
-           this.capture.release();
-
+        {
+            // release the camera
+            this.capture.release();
+        }
     }
 
-
-    private void updateImageView(ImageView view, Image image) {
+    /**
+     * Update the {@link ImageView} in the JavaFX main thread
+     *
+     * @param view
+     *            the {@link ImageView} to update
+     * @param image
+     *            the {@link Image} to show
+     */
+    private void updateImageView(ImageView view, Image image)
+    {
         Untils.onFXThread(view.imageProperty(), image);
-//        System.out.println("hello");
     }
 
+    /**
+     * On application close, stop the acquisition from the camera
+     */
     protected void setClosed()
     {
         this.stopAcquisition();
     }
 
 }
-
